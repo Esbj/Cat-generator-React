81aa5eb67ba55d8c4280de1dbd5825120bc631f1
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-08-30 11:08:25 +0200

    Added SelectCats to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import SelectCats from './SelectCats/SelectCats'
+
 
 function App() {
+  const fetchCats = () => {
+
+  }
   return (
     <>
       <div id="header">
         <h2>Welcome to the</h2>
         <h1>CAT GENERATOR</h1>
       </div>
+      <SelectCats fetchCats={fetchCats} />
     </>
   )
 }
