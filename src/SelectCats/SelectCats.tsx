e4972b69ebbe32909437f31ccbc3a634eb746b93
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-08-30 11:26:41 +0200

    checkboxes and styling for SelectCats

--- src/SelectCats/SelectCats.tsx
+++ src/SelectCats/SelectCats.tsx
@@ -1,27 +1,33 @@
 import React from 'react'
+import './SelectCats.scss'
 type Props = {
   fetchCats: () => void
 }
 export default function SelectCats({ fetchCats }: Props) {
-  fetchCats()
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    fetchCats()
+  }
   return (
-    <div>
-      <h3>Select what kind of cats you want
-      </h3>
-      <input type="checkbox" name="hats" id="hats" />
-      <label htmlFor="hats">🎩</label>
-      <input type="checkbox" name="space" id="space" />
-      <label htmlFor="space">🌌</label>
-      <input type="checkbox" name="sunglasses" id="sunglasses" />
-      <label htmlFor="sunglasses">🕶️</label>
-      <input type="checkbox" name="boxes" id="boxes" />
-      <label htmlFor="boxes">📦</label>
-      <input type="checkbox" name="ties" id="ties" />
-      <label htmlFor="ties">👔</label>
-      <input type="checkbox" name="sinks" id="sinks" />
-      <label htmlFor="sinks">🚿</label>
-      <input type="checkbox" name="clothes" id="clothes" />
-      <label htmlFor="clothes">👚</label>
-    </div>
+    <form onSubmit={(handleSubmit)}>
+      <h3>Select what kinds of cats you want</h3>
+      <div>
+        <input type="checkbox" name="hats" id="hats" />
+        <label htmlFor="hats">🎩</label>
+        <input type="checkbox" name="space" id="space" />
+        <label htmlFor="space">🌌</label>
+        <input type="checkbox" name="sunglasses" id="sunglasses" />
+        <label htmlFor="sunglasses">🕶️</label>
+        <input type="checkbox" name="boxes" id="boxes" />
+        <label htmlFor="boxes">📦</label>
+        <input type="checkbox" name="ties" id="ties" />
+        <label htmlFor="ties">👔</label>
+        <input type="checkbox" name="sinks" id="sinks" />
+        <label htmlFor="sinks">🚿</label>
+        <input type="checkbox" name="clothes" id="clothes" />
+        <label htmlFor="clothes">👚</label>
+      </div>
+      <button type="submit">Get cats</button>
+    </form>
   )
 }
